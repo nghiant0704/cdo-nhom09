df8cf15a0d3ef9d12453a577542e74763f824e16
nghiant7497 <nghiant7497@gmail.com>
2018-04-19 15:32:02 +0700

    thêm Model và View cho phần Admin

---
+++ application/models/Admin_model.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ThisPC
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin_model extends MY_Model
+{
+    var $table = 'ADMIN';
+}
