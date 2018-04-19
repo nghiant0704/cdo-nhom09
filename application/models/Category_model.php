0ac0d854b48bb9fc3ace76c575d98092a5b1f887
nghiant7497 <nghiant7497@gmail.com>
2018-04-19 16:15:00 +0700

    Revert "Revert "trang quản lý Danh mục sản phẩm""
    This reverts commit e578dec677ea0c5a5425cd63f8fac962feed6ced.

---
+++ application/models/Category_model.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ThisPC
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Category_model extends MY_Model
+{
+    var $table = 'CATEGORY';
+}
