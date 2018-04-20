fbbf0e5946ed1bdb9335937e87336efaebd07ebd
nghiant7497 <nghiant7497@gmail.com>
2018-04-20 17:36:50 +0700

    thêm Model cho bảng Order, Transaction và Comment

---
+++ application/models/Comment_model.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Comment_model extends MY_Model
+{
+    var $table = 'COMMENTS';
+}
---
+++ application/models/Transaction_model.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class  Transaction_model extends MY_Model
+{
+    var $table = 'TRANSACTIONS';
+}
